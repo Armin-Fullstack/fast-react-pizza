65e59d2f7d1b428c373a9101c4db871622438186
Armin Hossein Zadeh <Armin.Fullstack@gmail.com>
2023-10-11 19:54:00 +0330

    feat: fetched Menu with React Router Loader

--- src/features/menu/Menu.tsx
+++ src/features/menu/Menu.tsx
@@ -1,5 +1,19 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
+
 function Menu() {
-  return <h1>Menu</h1>;
+  const menu = useLoaderData();
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem key={pizza.id} pizza={pizza} />
+      ))}
+    </ul>
+  );
 }
 
+export async function loader() {
+  return await getMenu();
+}
 export default Menu;
