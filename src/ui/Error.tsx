73fa14520572d210c1ad2da1557df5f34bc6e791
Armin Hossein Zadeh <Armin.Fullstack@gmail.com>
2023-10-11 21:27:18 +0330

    feat: fetched Order

--- src/ui/Error.tsx
+++ src/ui/Error.tsx
@@ -3,7 +3,7 @@ import { useNavigate, useRouteError } from "react-router-dom";
 function Error() {
   const navigate = useNavigate();
   const error = useRouteError();
-
+  console.log(error);
   return (
     <div>
       <h1>Something went wrong 😢</h1>
