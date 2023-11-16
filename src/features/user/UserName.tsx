e82c02afc73f77e0a27745d1771c1962f6bd6d6a
Armin <Armin.Fullstack@gmail.com>
2023-11-16 08:48:08 +0330

    feat: modeled the User state with Redux Toolkit

--- src/features/user/UserName.tsx
+++ src/features/user/UserName.tsx
@@ -1,3 +1,8 @@
+import { useSelector } from "react-redux";
+
 export default function UserName() {
-  return <p className="hidden text-sm font-semibold md:block">Armin</p>;
+  const userName = useSelector((state) => state.user.userName);
+  if (!userName) return;
+
+  return <p className="hidden text-sm font-semibold md:block">{userName}</p>;
 }
